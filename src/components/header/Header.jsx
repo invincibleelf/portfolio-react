6d2dc30d24b64715fb7dcf1d6cb84da1201bc20c
Sharad Shrestha <invinciblesharad@gmail.com>
2021-06-16 13:07:05 +1000

    Hide and show header on scroll event

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
 import "./header.css";
 import mainlogo from "../../assests/images/main-logo.jpg";
-import {introduction} from "../../portfolio";
+import { introduction } from "../../portfolio";
 
 
 export default class Header extends Component {
 
+    state = {
+        display: "flex",
+        lastScrollY: 0
+    }
+
+    componentWillMount() {
+        window.addEventListener('scroll', this.handleScroll)
+    }
+
+    handleScroll = () => {
+        const { lastScrollY } = this.state;
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY > lastScrollY) {
+            this.setState({ display: "none"});
+        } else {
+            this.setState({ display: "flex" });
+        }
+        this.setState({ lastScrollY: currentScrollY });
+
+    };
+
     render() {
-        
+
         return (
-            <header>
+            <header style={{display:this.state.display}}>
                 <div className="title">
                     <img src={mainlogo} width='75px' alt="main logo" />
                     <a href='/'>
